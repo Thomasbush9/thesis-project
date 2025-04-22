c373df2d76d3429b49c1b4a67027b36056eedea5
thomas bush <thomasbush52@gmail.com>
2025-04-22 17:37:06 +0200

    autoencoder trainer, vae handles patches

--- denoising/models.py
+++ denoising/models.py
@@ -1,74 +1,47 @@
-# %%
+#%%
 import torch as t
 import numpy as np
 import einops
 from einops.layers.torch import Rearrange
 from torch.nn import Conv2d, ConvTranspose2d, Sequential, ReLU, BatchNorm2d, Linear
 from torch import Tensor, nn
-# %%
+#%%
 # ==== Baseline AutoEncoder ====
-
+# img = c, 64, 64
 class AutoEncoder(nn.Module):
     def __init__(self, latent_dim_size:int, hidden_dim_size:int):
         """Creates Encoder and Decoder modules"""
         super().__init__()
         self.latent = latent_dim_size
         self.hidden = hidden_dim_size
-        self.feature_shape = None
+        c2l = Rearrange('b c h w -> b (c h w)')
+        l2c = Rearrange('b (c h w) -> b c h w', c=32, h=16, w=16)
+
         #build modules:
-        self.encoder_conv = Sequential(
+        self.encoder = Sequential(
             Conv2d(in_channels=1, out_channels=16, kernel_size=4, stride=2, padding=1),
             ReLU(),
             BatchNorm2d(num_features=16),
-            Conv2d(in_channels=16, out_channels=32, kernel_size=4, stride=2, padding=1),
-        )
-        self.encoderfc = Sequential(
-            Linear(in_features=self.feature_shape, out_features=self.hidden),
+            Conv2d(in_channels=16, out_channels=32, kernel_size=4, stride=2, padding=1), 
+            ReLU(),
+            c2l,
+            Linear(in_features=32*16*16, out_features=self.hidden),
             ReLU(),
             Linear(in_features=self.hidden, out_features=self.latent)
-        )
-        self.decoder_fc = Sequential(
+             )
+        self.decoder = Sequential(
             Linear(self.latent, self.hidden),
             ReLU(),
-            Linear(self.hidden, 32*7*7),
-            ReLU()
-        )
-        self.decoder_conv = Sequential(
+            Linear(self.hidden, 32*16*16),
+            ReLU(),
+            l2c,
             ConvTranspose2d(32, 16, 4, 2, 1),
             BatchNorm2d(16),
             ReLU(),
             ConvTranspose2d(16, 1, 4, 2, 1)
         )
     def forward(self, x:Tensor)->Tensor:
-
-        xf = self.encoder_conv(x)
-        self.feature_shape = xf.shape[1:] #(c, h w)
-        print(self.feature_shape)
-        xf = xf.view(xf.size(0), -1)
-        print(xf.shape)
-        z = self.encoderfc(xf)
-
-        z_fc = self.decoder_fc(z)
-        z_fc = z_fc.view(-1, *self.feature_shape)  
-        x_prime = self.decoder_conv(z_fc)
-        x_prime = self.decoder_conv(z)
+        z = self.encoder(x)
+        x_prime = self.decoder(z)
         return x_prime
-    
-# === Variational Autoencoder === 
 
-
-#%%
-model = AutoEncoder(latent_dim_size=5, hidden_dim_size=128)
-#%%
-# get data:
-data_path = '/Users/thomasbush/Documents/DSS_Tilburg/data/keyframes/_2025-04-22 00:25:47.432414_keyframes.pth'
-data = t.load(data_path, map_location='cpu', weights_only=False)
-idx = data['keyframe_idx']
-keyframes = data['keyframes']
-#%%
-
-keyframes = keyframes.type(t.float)
-model.forward(keyframes[0:2].unsqueeze(dim=1))
-
-
-# %%
--- denoising/train.py
+++ denoising/train.py
@@ -3,37 +3,66 @@ import torch as t
 from torch.utils.data import DataLoader, Dataset, Subset
 from tqdm import tqdm
 from dataclasses import dataclass
-from utils import loadVideoArray
+from utils import loadVideoArray, generatePatches, reconstructFromPatches
 from torch import Tensor
 import numpy as np
 import argparse
 import torch.nn as nn
 from models import AutoEncoder
-
-
-# TODO create holdout data function,  train from keyframes, log if needed, save model if needed and model config 
+import torch
+import einops
+from torch.utils.data import random_split
 
 class CustomDataset(Dataset):
-    def __init__(self, data:np.ndarray):
-        self.frames = Tensor(data)
+    def __init__(self, data:Tensor):
+        self.frames = data
 
     def __len__(self):
         return self.frames.shape[0]
     
     def __getitem__(self, idx):
-        img = self.frames[idx]  # shape: [H, W]
+        img = self.frames[idx]  
         return img
 
 
-# create data loader
+def buildDatasetFromTensor(input:Tensor, dim:int, train_ratio:float=.8)->tuple:
+
+    assert len(input.shape) == 3, 'Input tensor must be frame h w'
+    patches_frames, original_shape, padding = generatePatches(input, dim)
+
+    tot_patches = einops.rearrange(patches_frames, 'f p h w -> (f p ) 1 h w', h=dim, w=dim).type(torch.float)
+
+    dataset = CustomDataset(tot_patches)
+
+    # Split into train/test
+    total_len = len(dataset)
+    train_len = int(train_ratio * total_len)
+    test_len = total_len - train_len
+
+    train_dataset, test_dataset = random_split(dataset, [train_len, test_len])
+
+    return train_dataset, test_dataset, original_shape, padding
+
+
+def getHoldoutData(testset:Dataset, num_data:int=40)->Tensor:
+    """Ideally get a whole img not in training"""
+
+    assert len(testset) % num_data==0, f'{num_data} makes a whole img'
+    # extract imgs
+    imgs = next(iter(DataLoader(test_dataset, batch_size=num_data, shuffle=False)))
+    return imgs 
+
 @dataclass
 class AutoencoderArgs:
-    latent_dim_size: int = 5
+    trainset: Dataset
+    testset: Dataset
+    holdoutData: Tensor
+
+    latent_dim_size: int = 16
     hidden_dim_size: int = 128
 
     # data / training
-    dataset: Literal["MNIST", "CELEB"] = "MNIST"
-    batch_size: int = 64
+    batch_size: int = 32
     epochs: int = 10
     lr: float = 1e-3
     betas: tuple[float, float] = (0.5, 0.999)
@@ -49,7 +78,8 @@ class AutoencoderTrainer:
     def __init__(self, args: AutoencoderArgs, device:str):
         self.args = args
         self.device = device
-        self.trainset = get_dataset(args.dataset)
+        self.trainset = args.trainset
+        self.HOLDOUT_DATA = args.holdoutData
         self.trainloader = DataLoader(self.trainset, batch_size=args.batch_size, shuffle=True)
         self.model = AutoEncoder(
             latent_dim_size=args.latent_dim_size,
@@ -63,8 +93,6 @@ class AutoencoderTrainer:
         """
         Performs a training step on the batch of images in `img`. Returns the loss. Logs to wandb if enabled.
         """
-        # pass imgs to model
-
         pred = self.model.forward(img)
         loss = self.loss(pred, img)
         loss.backward()
@@ -77,18 +105,17 @@ class AutoencoderTrainer:
 
         return loss
 
-
     @t.inference_mode()
     def log_samples(self) -> None:
         """
         Evaluates model on holdout data, either logging to weights & biases or displaying output.
         """
         assert self.step > 0, "First call should come after a training step. Remember to increment `self.step`."
-        output = self.model(HOLDOUT_DATA)
-        if self.args.use_wandb:
-            wandb.log({"images": [wandb.Image(arr) for arr in output.cpu().numpy()]}, step=self.step)
-        else:
-            display_data(t.concat([HOLDOUT_DATA, output]), nrows=2, title="AE reconstructions")
+        output = self.model(self.HOLDOUT_DATA.to(self.device).float())
+        # if self.args.use_wandb:
+        #     wandb.log({"images": [wandb.Image(arr) for arr in output.cpu().numpy()]}, step=self.step)
+        # else:
+        #     display_data(t.concat([HOLDOUT_DATA, output]), nrows=2, title="AE reconstructions")
 
     def train(self) -> AutoEncoder:
         """Performs a full training run."""
@@ -97,29 +124,22 @@ class AutoencoderTrainer:
             wandb.init(project=self.args.wandb_project, name=self.args.wandb_name)
             wandb.watch(self.model)
 
-        # YOUR CODE HERE - iterate over epochs, and train your model
-
         for epoch in range(self.args.epochs):
 
             progress_bar = tqdm(self.trainloader, total=int(len(self.trainloader)), ascii=True)
 
-            for imgs, _ in progress_bar:
+            for imgs in progress_bar:
                 imgs = imgs.to(self.device)
                 loss = self.training_step(imgs)
                 progress_bar.set_description(f"{epoch=:02d}, {loss=:.4f}, step={self.step:05d}")
-                #pbar.set_postfix(loss=f"{loss:.3f}", ex_seen=f"{self.step=:06}")
                 # log every 250 steps
                 if self.step % self.args.log_every_n_steps == 0:
-
                   self.log_samples()
-
 
         if self.args.use_wandb:
             wandb.finish()
 
         return self.model
-
-
 
 if __name__ == '__main__':
 
@@ -129,4 +149,18 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
     data_path = args.data_path
+
+    data = torch.load(data_path, map_location='cpu', weights_only=False)
+    keyframes = data['keyframes']
+    idx = data['keyframe_idx']
+
+    train_dataset, test_dataset, orig_shape, padding = buildDatasetFromTensor(keyframes, dim=64)
+    args_trainer = AutoencoderArgs(trainset=train_dataset, testset=test_dataset, holdoutData=getHoldoutData(test_dataset))
+
+# === Start Trainign ===
+    trainer = AutoencoderTrainer(args_trainer, device='mps')
+    autoencoder = trainer.train()
+
+
+
     
